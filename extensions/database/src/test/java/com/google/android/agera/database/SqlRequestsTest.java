bbf914627a36ff5b61828ae8842adf4d3309c109
Magnus Ernstsson <magnus@ernstsson.net>
2016-05-14 18:40:32 +0100

    Added sql request compiler tests

--- extensions/database/src/test/java/com/google/android/agera/database/SqlRequestsTest.java
+++ extensions/database/src/test/java/com/google/android/agera/database/SqlRequestsTest.java
@@ -21,11 +21,18 @@ import static com.google.android.agera.database.SqlRequests.sqlRequest;
 import static com.google.android.agera.database.SqlRequests.sqlUpdateRequest;
 import static com.google.android.agera.database.test.matchers.HasPrivateConstructor.hasPrivateConstructor;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasToString;
 import static org.hamcrest.Matchers.isEmptyOrNullString;
 import static org.hamcrest.Matchers.not;
 import static org.robolectric.annotation.Config.NONE;
+
+import com.google.android.agera.database.SqlRequestCompilerStates.DBArgumentCompile;
+import com.google.android.agera.database.SqlRequestCompilerStates.DBColumn;
+import com.google.android.agera.database.SqlRequestCompilerStates.DBColumnConflictCompile;
+import com.google.android.agera.database.SqlRequestCompilerStates.DBCompile;
+import com.google.android.agera.database.SqlRequestCompilerStates.DBSql;
+import com.google.android.agera.database.SqlRequestCompilerStates.DBTable;
+import com.google.android.agera.database.SqlRequestCompilerStates.DBWhereCompile;
 
 import nl.jqno.equalsverifier.EqualsVerifier;
 import org.junit.Before;
@@ -100,6 +107,109 @@ public final class SqlRequestsTest {
   @Test
   public void shouldCreateStringRepresentationForUpdate() {
     assertThat(sqlUpdateRequest, hasToString(not(isEmptyOrNullString())));
+  }
+
+  @Test(expected = IllegalStateException.class)
+  public void shouldThrowExceptionForReuseOfCompilerOfCompile() {
+    final DBColumnConflictCompile<SqlInsertRequest, ?> incompleteRequest =
+        sqlInsertRequest()
+            .table(TABLE_2)
+            .column("column", "value");
+    incompleteRequest.compile();
+
+    incompleteRequest.compile();
+  }
+
+  @Test(expected = IllegalStateException.class)
+  public void shouldThrowExceptionForReuseOfCompilerOfFailOnConflict() {
+    final DBColumnConflictCompile<SqlInsertRequest, ?> incompleteRequest =
+        sqlInsertRequest()
+            .table(TABLE_2)
+            .column("column", "value");
+    incompleteRequest.failOnConflict().compile();
+
+    incompleteRequest.failOnConflict();
+  }
+
+  @Test(expected = IllegalStateException.class)
+  public void shouldThrowExceptionForReuseOfCompilerOfIgnoreOnConflict() {
+    final DBColumnConflictCompile<SqlInsertRequest, ?> incompleteRequest =
+        sqlInsertRequest()
+            .table(TABLE_2)
+            .column("column", "value");
+    incompleteRequest.ignoreOnConflict().compile();
+
+    incompleteRequest.ignoreOnConflict();
+  }
+
+  @Test(expected = IllegalStateException.class)
+  public void shouldThrowExceptionForReuseOfCompilerOfReplaceOnConflict() {
+    final DBColumnConflictCompile<SqlInsertRequest, ?> incompleteRequest =
+        sqlInsertRequest()
+            .table(TABLE_2)
+            .column("column", "value");
+    incompleteRequest.replaceOnConflict().compile();
+
+    incompleteRequest.replaceOnConflict();
+  }
+
+  @Test(expected = IllegalStateException.class)
+  public void shouldThrowExceptionForReuseOfCompilerOfColumn() {
+    final DBColumn<DBColumnConflictCompile<SqlInsertRequest, ?>> incompleteRequest =
+        sqlInsertRequest()
+            .table(TABLE_2);
+    incompleteRequest.column("column", "value").compile();
+
+    incompleteRequest.column("column", "value");
+  }
+
+  @Test(expected = IllegalStateException.class)
+  public void shouldThrowExceptionForReuseOfCompilerOfEmptyColumn() {
+    final DBColumn<DBColumnConflictCompile<SqlInsertRequest, ?>> incompleteRequest =
+        sqlInsertRequest()
+            .table(TABLE_2);
+    incompleteRequest.emptyColumn("column").compile();
+
+    incompleteRequest.emptyColumn("column");
+  }
+
+  @Test(expected = IllegalStateException.class)
+  public void shouldThrowExceptionForReuseOfCompilerOfTable() {
+    final DBTable<DBColumn<DBColumnConflictCompile<SqlInsertRequest, ?>>>
+        incompleteRequest = sqlInsertRequest();
+    incompleteRequest.table(TABLE).column("column", "value").compile();
+
+    incompleteRequest.table(TABLE);
+  }
+
+  @Test(expected = IllegalStateException.class)
+  public void shouldThrowExceptionForReuseOfCompilerOfSql() {
+    final DBSql<DBArgumentCompile<SqlRequest, DBArgumentCompile<SqlRequest, DBCompile<SqlRequest>>>>
+        incompleteRequest = sqlRequest();
+    incompleteRequest.sql("sql").compile();
+
+    incompleteRequest.sql("sql");
+  }
+
+  @Test(expected = IllegalStateException.class)
+  public void shouldThrowExceptionForReuseOfCompilerOfArguments() {
+    final DBArgumentCompile<SqlRequest, DBArgumentCompile<SqlRequest, DBCompile<SqlRequest>>>
+        incompleteRequest = sqlRequest().sql("sql");
+    incompleteRequest.arguments("arg", "arg").compile();
+
+    incompleteRequest.arguments("arg", "arg");
+  }
+
+
+  @Test(expected = IllegalStateException.class)
+  public void shouldThrowExceptionForReuseOfCompilerOfWhere() {
+    final DBWhereCompile<SqlDeleteRequest,
+        DBArgumentCompile<SqlDeleteRequest, DBCompile<SqlDeleteRequest>>>
+        incompleteRequest = sqlDeleteRequest()
+        .table(TABLE);
+    incompleteRequest.where("column=a").compile();
+
+    incompleteRequest.where("column=a");
   }
 
   @Test
